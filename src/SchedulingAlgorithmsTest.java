295aad56226c5ab2838697bcf2d3ff1bae64fec0
Alix Leon <alixlm19@hotmail.com>
2019-12-07 16:47:00 -0500

    Added comments

--- src/SchedulingAlgorithmsTest.java
+++ src/SchedulingAlgorithmsTest.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class SchedulingAlgorithmsTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SchedulingAlgorithms sa = new SchedulingAlgorithms(5000, 0, 1000);
-		System.out.println(sa.FCFS(50));
-		System.out.println(sa.SSTF(50));
-		System.out.println(sa.SCAN(50));
+		SchedulingAlgorithms sa = new SchedulingAlgorithms(5000, 0, 1000);	//Initialize the disk scheduling algorithms class
+		
+		
+		System.out.println(sa.FCFS(50));	//First Come, First Serve algorithm total head movement
+		System.out.println(sa.SSTF(50));	//Shortest Seek Time First algorithm total head movement
+		System.out.println(sa.SCAN(50));	//Scan algorithm total head movement
 
 	}
 
